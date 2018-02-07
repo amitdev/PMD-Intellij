90f956b4ef49049cc242bdae0c01fe4f0b9f80b0
Amit Dev <aranjitdev@atlassian.com>
2018-02-07 21:30:02 +1100

    Fixes #44

--- src/com/intellij/plugins/bodhi/pmd/actions/PreDefinedMenuGroup.java
+++ src/com/intellij/plugins/bodhi/pmd/actions/PreDefinedMenuGroup.java
@@ -1,21 +1,15 @@
 package com.intellij.plugins.bodhi.pmd.actions;
 
 import com.intellij.openapi.actionSystem.*;
-import com.intellij.openapi.keymap.Keymap;
-import com.intellij.openapi.components.ProjectComponent;
 import com.intellij.plugins.bodhi.pmd.PMDUtil;
 import com.intellij.plugins.bodhi.pmd.PMDInvoker;
 import com.intellij.plugins.bodhi.pmd.PMDProjectComponent;
-import com.intellij.util.xml.ui.actions.DefaultAddAction;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Properties;
 import java.io.IOException;
 
 import net.sourceforge.pmd.util.ResourceLoader;
-import net.sourceforge.pmd.RuleSetNotFoundException;
-
-import javax.swing.*;
 
 /**
  * This ActionGroup defines the actions for pre defined rulesets that
@@ -51,25 +45,23 @@ public class PreDefinedMenuGroup extends ActionGroup {
         Properties props = new Properties();
         try {
             //Load the property file which has all the rulesets.
-            props.load(ResourceLoader.loadResourceAsStream(RULESETS_PROPERTY_FILE));
+            props.load(new ResourceLoader(getClass().getClassLoader()).loadResourceAsStream(RULESETS_PROPERTY_FILE));
             String[] rulesetFilenames = props.getProperty(RULESETS_FILENAMES).split(PMDInvoker.RULE_DELIMITER);
 
             //We have 'All' rules in addition to the rulesets
-            //children = new AnAction[rulesetFilenames.length+1];
             //First one is 'All'
             children.removeAll();
             children.add(action);
 
             for (int i=0; i < rulesetFilenames.length; ++i) {
-                int start = rulesetFilenames[i].indexOf('/');
-                int end = rulesetFilenames[i].indexOf('.');
-                final String name = rulesetFilenames[i].substring(start+1, end);
-                allRules += name;
+                final String ruleFileName = rulesetFilenames[i];
+                final String ruleName = PMDUtil.getRuleName(ruleFileName);
+                allRules += ruleFileName;
                 allRules += (i == rulesetFilenames.length - 1) ? "" : PMDInvoker.RULE_DELIMITER;
-                AnAction ruleAction = new AnAction(name) {
+                AnAction ruleAction = new AnAction(ruleName) {
                     public void actionPerformed(AnActionEvent e) {
-                        PMDInvoker.getInstance().runPMD(e, name, false);
-                        getComponent().setLastRunRules(name);
+                        PMDInvoker.getInstance().runPMD(e, ruleFileName, false);
+                        getComponent().setLastRunRules(ruleFileName);
                     }
                 };
                 children.add(ruleAction);
